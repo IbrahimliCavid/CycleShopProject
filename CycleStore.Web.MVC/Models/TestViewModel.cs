8a8b26f6053f3b320bab5263e7de43c2c254969d
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-06-19 02:26:19 +0400

    Upload CkEditor And TestViewComponent add

---
+++ CycleStore.Web.MVC/Models/TestViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CycleStore.Web.MVC.Models
+{
+    public class TestViewModel
+    {
+        public int Count { get; set; }  
+    }
+}
---
+++ CycleStore.Web.MVC/ViewComponents/TestViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Buisness.Abstract;
+using CycleStore.Web.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CycleStore.Web.MVC.ViewComponents
+{
+    public class TestViewComponent : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public TestViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = _categoryService.GetAll().Data.Count();
+
+            return View(new TestViewModel()
+            {
+                Count = count
+            });
+        }
+    }
+}
